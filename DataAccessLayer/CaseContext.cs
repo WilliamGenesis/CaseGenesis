81b89b52176bc5ed94bdc139877c939666c19f21
William Bienfait <wbienfait@genesis-consult.com>
2020-03-20 13:03:01 +0100

    Add - context + db placeholder

---
+++ DataAccessLayer/CaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer
+{
+	public class CaseContext : DbContext
+	{
+		public CaseContext(DbContextOptions options)
+			: base(options)
+		{
+		}
+
+		public DbSet<Contact> Employees { get; set; }
+	}
+}
