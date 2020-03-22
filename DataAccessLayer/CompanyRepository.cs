00dc438e159ff9d1b0d7acf7f028f6bf45a1a5c0
William Bienfait <wbienfait@genesis-consult.com>
2020-03-22 13:02:41 +0100

    Add - Company data access layer

--- DataAccessLayer/CompanyRepository.cs
+++ DataAccessLayer/CompanyRepository.cs
@@ -1,35 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using Entities;
 
 namespace DataAccessLayer
 {
 	public class CompanyRepository : ICompanyRepository
 	{
+		IFakeObjectGenerator _fakeObjectGenerator;
+
+		public CompanyRepository(IFakeObjectGenerator fakeObjectGenerator)
+		{
+			_fakeObjectGenerator = fakeObjectGenerator;
+		}
+
 		public Guid CreateCompany(Company company)
 		{
-			throw new NotImplementedException();
+			company.Id = Guid.NewGuid();
+
+			_fakeObjectGenerator.Companies.Add(company);
+
+			return company.Id;
 		}
 
 		public bool DeleteCompany(Guid companyId)
 		{
-			throw new NotImplementedException();
+			var original = _fakeObjectGenerator.Companies.FirstOrDefault(comp => comp.Id == companyId);
+
+			if (original == null)
+				return false;
+
+			_fakeObjectGenerator.Companies.Remove(original);
+			return true;
 		}
 
 		public Company GetCompany(Guid companyId)
 		{
-			throw new NotImplementedException();
+			return _fakeObjectGenerator.Companies.FirstOrDefault(company => company.Id == companyId);
 		}
 
 		public Guid UpdateCompany(Company company)
 		{
-			throw new NotImplementedException();
+			var original = _fakeObjectGenerator.Companies.FirstOrDefault(comp => comp.Id == company.Id);
+
+			if (original == null)
+				throw new Exception("The contact you are trying to update does not exist");
+
+			original = company;
+
+			return company.Id;
 		}
 
 		public Guid UpsertCompany(Company company)
 		{
-			throw new NotImplementedException();
+			var original = _fakeObjectGenerator.Companies.FirstOrDefault(comp => comp.Id == company.Id);
+
+			if (original != null)
+				original = company;
+
+			company.Id = Guid.NewGuid();
+			_fakeObjectGenerator.Companies.Add(company);
+
+			return company.Id;
 		}
 	}
 }
